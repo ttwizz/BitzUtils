5e304bee00e47b668a9ebccd088c7ddfd0b95981
NougatBitz <67209080+NougatBitz@users.noreply.github.com>
2022-03-24 03:11:30 +0100

    Update Celery Funcs.lua

--- Celery Funcs.lua
+++ Celery Funcs.lua
@@ -1,3 +1,4 @@
+--// Init
 assert(rnet, "Celery not Injected/env not created.");
 
 local runService = game:GetService("RunService");
@@ -32,3 +33,26 @@ end
 getgenv().send_raw = send_raw
 getgenv().toggle_sid = toggle_sid
 getgenv().capture = capture
+
+
+--[[
+    Example Usage:
+
+    capture(this acts like a normal RBXSignal): 
+        capture:Connect(function(packet)
+            rconsoleprint("\n")
+            local str_bytes = rnet.tostring(packet.bytes)
+            str_bytes = str_bytes:sub(1, #str_bytes - 1)
+            rconsoleprint("\n")
+            rconsoleprint("Packet recieved: " .. tostring(packet.sid))
+            rconsoleprint(str_bytes)
+        end)
+
+
+    toggle_sid(sid):
+        toggle_sid(X)
+
+    send_raw(str_bytes):
+        send_raw("XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX")
+
+--]]
