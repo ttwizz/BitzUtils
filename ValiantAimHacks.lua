639a4c93c0bef9780aa8ce7e6bb460e98769f6bf
NougatBitz <67209080+NougatBitz@users.noreply.github.com>
2022-05-02 16:24:23 +0200

    Update ValiantAimHacks.lua

--- ValiantAimHacks.lua
+++ ValiantAimHacks.lua
@@ -1,5 +1,3 @@
--- // New version: See ./Aiming/SilentAim.lua
-
 if getgenv().ValiantAimHacks then return getgenv().ValiantAimHacks end
 
 -- // Services
@@ -37,10 +35,10 @@ local FindFirstChild = Instancenew("Part").FindFirstChild
 getgenv().ValiantAimHacks = {
     SilentAimEnabled = true,
     ShowFOV = true,
-    FOVSides = 12,
+    FOVSides = 48,
     VisibleCheck = true,
-    TeamCheck = true,
-    FOV = 60,
+    TeamCheck = false,
+    FOV = 120,
     HitChance = 100,
     Selected = LocalPlayer,
     SelectedPart = nil,
@@ -371,69 +369,3 @@ Heartbeat:Connect(function()
 end)
 
 return ValiantAimHacks
-
---[[
-Examples:
-
---// Namecall Version // --
--- // Metatable Variables
-local mt = getrawmetatable(game)
-local backupindex = mt.__index
-setreadonly(mt, false)
-
--- // Load Silent Aim
-local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
-
--- // Hook
-mt.__namecall = newcclosure(function(...)
-    -- // Vars
-    local args = {...}
-    local method = getnamecallmethod()
-
-    -- // Checks
-    if (method == "FireServer") then
-        if (args[1].Name == "RemoteNameHere") then
-            -- change args
-
-            -- // Return changed arguments
-            return backupnamecall(unpack(args))
-        end
-    end
-
-    -- // Return
-    return backupnamecall(...)
-end)
-
--- // Revert Metatable readonly status
-setreadonly(mt, true)
-
--- // Index Version // --
--- // Metatable Variables
-local mt = getrawmetatable(game)
-local backupindex = mt.__index
-setreadonly(mt, false)
-
--- // Load Silent Aim
-local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
-
--- // Hook
-mt.__index = newcclosure(function(t, k)
-    -- // Check if it trying to get our mouse's hit or target
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target")) then
-        -- // If we can use the silent aim
-        if (ValiantAimHacks.checkSilentAim()) then
-            -- // Vars
-            local TargetPart = ValiantAimHacks.SelectedPart
-
-            -- // Return modded val
-            return (k == "Hit" and TargetPart.CFrame or TargetPart)
-        end
-    end
-
-    -- // Return
-    return backupindex(t, k)
-end)
-
--- // Revert Metatable readonly status
-setreadonly(mt, true)
-]]
